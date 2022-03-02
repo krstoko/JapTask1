f654b5b8ec09ed34c1f6c971391179535fcad1c4
Krsto Kostic <krstoko@maestralsolutions.com>
2022-03-03 00:13:17 +0100

    Adding recipe list and recipe box

---
+++ frontend/src/Components/Recipes/RecipeBox.js
@@ -0,0 +1,39 @@
+import { Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const RecipeBox = ({recipe}) => {
+    const navigate = useNavigate()
+    const style = {
+        display: "flex",
+        justifyContent: "space-between"
+    }
+    const onRecipeClick = () =>{
+        navigate(`/recipe/${recipe.name}`)
+    }
+
+  return (
+    <Grid item xs={12} sm={6} md={4}>
+      <Card>
+      <CardActionArea onClick={onRecipeClick}>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent sx={style}>
+          <Typography gutterBottom variant="h5" component="div">
+            {recipe.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {recipe.price}$
+          </Typography>
+        </CardContent>
+        </CardActionArea>
+      </Card>
+    </Grid>
+  );
+};
+
+export default RecipeBox;
--- frontend/src/Components/Recipes/RecipesBody.js
+++ frontend/src/Components/Recipes/RecipesBody.js
@@ -1,17 +1,37 @@
 import { Box, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import SearchBar from "../ui-component/SearchBar";
-
+import RecipeList from "./RecipeList";
+import ButtonReusable from "../ui-component/ButtonReusable";
 const RecipesBody = () => {
   const style = {
     width: "100%",
   };
+  const [recipes, setRecipes] = useState([
+    { name: "Test1", price: 22 },
+    { name: "Test2", price: 23 },
+    { name: "Test3", price: 21 },
+    { name: "Test4", price: 24 },
+    { name: "Test5", price: 25 },
+    { name: "Test6", price: 26 },
+    { name: "Test7", price: 27 },
+    { name: "Test8", price: 21 },
+    { name: "Test9", price: 22 },
+  ]);
+
+  const onClickMoreHandler = () => {};
 
   return (
     <React.Fragment>
-      <Box display="flex" justifyContent="space-between" sx={style} mt={2} >
+      <Box display="flex" justifyContent="space-between" sx={style} mt={2}>
         <Typography variant="h5">Total recipes: 20</Typography>
         <SearchBar />
+      </Box>
+      <RecipeList recipes={recipes} />
+      <Box my={2} display="flex" justifyContent="center">
+        <ButtonReusable variant="outlined" onClick={onClickMoreHandler}>
+          Load more
+        </ButtonReusable>
       </Box>
     </React.Fragment>
   );
