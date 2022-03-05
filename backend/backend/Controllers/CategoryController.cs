2e47fabb7ffc5487f3342463f2ace7d2a8b43067
Krsto Kostic <krstoko@maestralsolutions.com>
2022-03-05 15:07:59 +0100

    Adding Controller for category and testing get method with dummy data

---
+++ backend/backend/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private static Category category = new Category();
+        [HttpGet]
+        public ActionResult<Category> get()
+        {
+            return Ok(category);
+        }
+
+    }
+}
